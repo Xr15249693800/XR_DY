6172efa5a623edf461a5edbe16c784eb573301a3
薛冉 <xueranit@xueranitdemacbook-pro.local>
2019-04-29 13:57:11 +0800

    初始化项目

---
+++ XR-DYZB/XR-DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XR-DYZB
+//
+//  Created by xueranit on 2019/4/29.
+//  Copyright © 2019 xr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
